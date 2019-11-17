82db4bf44487e66ec1e0bc3c03e357879cef638f
Waldir Borba Junior <wborbajr@gmail.com>
2019-11-17 08:09:36 -0300

    Initial Import

---
+++ src/routes/api.js
@@ -0,0 +1,31 @@
+exports.plugin = {
+    async register(server, options) {
+        server.route([
+            {
+                method: 'GET',
+                path: '/work',
+                options: {
+                    description: 'main request handler',
+                    notes: 'Returns a todo item by the id passed in the path',
+                    tags: ['api'], // ADD THIS TAG
+                    handler: async (request, h) => {
+                        return 'It\'s Works!';
+                    }
+                }
+            },
+            {
+                method: 'GET',
+                path: '/api/{id}',
+                options: {
+                    description: 'main request handler',
+                    handler: async (request, h) => {
+
+                        return h.view('detail_page', {'title': 'Detail Page', id: request.params.id});
+                    }
+                }
+            },        
+        ]);
+    },
+    version: require('../../package.json').version,
+    name: 'route-main'
+};
---
+++ src/server.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const Hapi = require('@hapi/hapi');
+const Inert = require('@hapi/inert');
+const Vision = require('@hapi/vision');
+const HapiSwagger = require('hapi-swagger');
+const config = require('../package.json');
+
+const init = async () => {
+
+    const server = Hapi.server({
+        port: 3000,
+        host: 'localhost'
+    });
+
+    const swaggerOptions = {
+        info: {
+                title: config.description + ' Documentation',
+                version: config.version,
+            },
+        };
+
+    await server.register([
+        Inert,
+        Vision,
+        {
+            plugin: HapiSwagger,
+            options: swaggerOptions
+        }
+    ]);
+
+    // API Route
+    await server.register({
+        plugin: require('./routes/api'),
+        // options: {
+        //     routes: {
+        //         prefix: '/api'
+        //     }
+        // }
+    });    
+
+    // await server.register({
+    //     plugin: require('your-route-folder/users'),
+    //     options: {
+    //         routes: {
+    //             prefix: '/user'
+    //         }
+    //     }
+    // });
+
+    await server.start();
+    console.log('%s Server %s running on %s', config.description, config.version, server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
